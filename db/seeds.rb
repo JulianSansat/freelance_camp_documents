c9ac7acd5428998f920a9627c50a42ebf0ef3406
JulianSansat <kerarchie@yahoo.com.ar>
2017-03-17 22:46:16 -0300

    rack cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |d|
+FreelanceDocument.create!(
+    title: "Document #{d}",
+    description: "É verão o ano todo vem cumpadi. Não vai dá não. Aqui nóis constrói fibra, não é água com músculo. Ele tá olhando pra gente. Vo derrubar tudo essas árvore do parque ibirapuera. Que não vai dá rapaiz, não vai dá essa porra.",
+    file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg"
+    )
+end
